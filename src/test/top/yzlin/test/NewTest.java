89ac70ac57b1f9aced806aff218955dbd183e32f
ay2008lnt <278475596@qq.com>
2019-05-22 22:51:04 +0800

    奇穴技能页面更新

--- src/test/top/yzlin/test/NewTest.java
+++ src/test/top/yzlin/test/NewTest.java
@@ -1,5 +1,23 @@
 package top.yzlin.test;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import top.yzlin.jx3strategystation.service.QiXueGroupService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:applicationConfig.xml"})
+@ActiveProfiles("dev")
 public class NewTest {
 
+    @Autowired
+    private QiXueGroupService qiXueGroupService;
+
+    @Test
+    public void test() {
+        System.out.println(qiXueGroupService.findQiXueGroup());
+    }
 }
