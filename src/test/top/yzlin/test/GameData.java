ead020d15587700980de9953c4a005b0823d30d5
yzlin <499680328@qq.com>
2019-06-02 02:55:17 +0800

    完善天策数据，跟进纯阳数据

--- src/test/top/yzlin/test/GameData.java
+++ src/test/top/yzlin/test/GameData.java
@@ -9,14 +9,32 @@ import org.springframework.util.ResourceUtils;
 import top.yzlin.jx3strategystation.entity.game.*;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class GameData {
     private Pattern pattern = Pattern.compile("class=\"font-100\">(?<content>.*?)</span>");
     private Pattern coolDownTimePattern = Pattern.compile("class=\"font-165\">(?<content>.*?)</span>");
+
+    @Test
+    public void createSkill() {
+        String[] skills = {"生太极", "吞日月", "碎星辰", "转乾坤", "剑冲阴阳", "八荒归元", "大道无术", "人剑合一", "无我无剑",
+                "三环套月", "万剑归宗", "紫气东来", "凭虚御风", "梯云纵", "坐忘无我", "化三清", "剑飞惊天", "行天道"};
+        List<Skill> skillList = Arrays.stream(skills).map(s -> {
+            Skill skill = new Skill();
+            skill.setName(s);
+            skill.setArm("短兵");
+            skill.setSkillTypes(new SkillType[0]);
+            skill.setDescribe(getDescribe(s));
+            return skill;
+        }).collect(Collectors.toList());
+        System.out.println(JSON.toJSONString(skillList));
+    }
 
     @Test
     public void getData() throws IOException {
@@ -50,6 +68,31 @@ public class GameData {
             }
         }
         System.out.println(JSON.toJSONString(menPai));
+    }
+
+    @Test
+    public void getQiXueData() throws IOException {
+        JSONObject netData = JSON.parseObject(Tools.sendGet("https://api.ipsfan.com/jx3qx/tc.json", ""));
+        Map<String, String> qiXueCollect = netData.getJSONArray("data")
+                .getJSONObject(1)
+                .getJSONArray("kungfuLevel")
+                .toJavaList(JSONObject.class)
+                .stream()
+                .flatMap(ja -> {
+                    List<JSONObject> kungfuSkills = ja.getJSONArray("kungfuSkills").toJavaList(JSONObject.class);
+                    List<JSONObject> forceSkills = ja.getJSONArray("forceSkills").toJavaList(JSONObject.class);
+                    forceSkills.forEach(jo -> jo.put("name", jo.getString("skillName")));
+                    return Stream.concat(kungfuSkills.stream(), forceSkills.stream());
+                }).collect(Collectors.toMap(k -> k.getString("name"), v -> v.getString("desc")));
+        String data = FileUtils.readFileToString(ResourceUtils.getFile("classpath:data/menpai/铁牢奇穴.json"), "utf-8");
+        JSONArray objects = JSON.parseArray(data);
+        List<QiXueGroup> qixues = objects.toJavaList(QiXueGroup.class);
+        for (QiXueGroup qixue : qixues) {
+            for (QiXue qiXue : qixue.getQiXues()) {
+                qiXue.setDescribe(qiXueCollect.get(qiXue.getName()));
+            }
+        }
+        System.out.println(JSON.toJSONString(qixues));
     }
 
     public void qixueGetDescribe() {
--- src/test/top/yzlin/test/TestUnit.java
+++ src/test/top/yzlin/test/TestUnit.java
@@ -5,6 +5,7 @@ import org.apache.commons.io.FileUtils;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,10 +13,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.util.ResourceUtils;
-import top.yzlin.jx3strategystation.entity.game.MenPai;
-import top.yzlin.jx3strategystation.entity.game.QiXue;
-import top.yzlin.jx3strategystation.entity.game.QiXueGroup;
-import top.yzlin.jx3strategystation.entity.game.SkillType;
+import top.yzlin.jx3strategystation.entity.game.*;
 import top.yzlin.jx3strategystation.service.ArticleService;
 import top.yzlin.jx3strategystation.service.CommentService;
 import top.yzlin.jx3strategystation.service.UserService;
@@ -42,37 +40,66 @@ public class TestUnit {
     @Autowired
     private SessionFactory sessionFactory;
 
+    @Test
+    public void test() {
+        Session session = sessionFactory.openSession();
+        MenPai menPai = session.get(MenPai.class, 171);
+        System.out.println(JSON.toJSONString(menPai));
+    }
 
     @Test
-    public void fileTest() throws IOException {
+    public void saveMenpai() throws IOException {
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
-        String s = FileUtils.readFileToString(ResourceUtils.getFile("classpath:data/霸刀门派.json"), "utf-8");
+        String s = FileUtils.readFileToString(ResourceUtils.getFile("classpath:data/menpai/天策门派.json"), "utf-8");
         MenPai menPai = JSON.parseObject(s).toJavaObject(MenPai.class);
         session.save(menPai);
         transaction.commit();
+    }
+
+    @Test
+    public void setSkillType() throws IOException {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        List<SkillType> skillTypeList = session.createQuery("from SkillType ", SkillType.class).list();
+        Map<String, SkillType> collect = skillTypeList.stream().collect(Collectors.toMap(SkillType::getTitle, v -> v));
+        String s = FileUtils.readFileToString(ResourceUtils.getFile("classpath:data/menpai/天策门派.json"), "utf-8");
+        MenPai menPai = JSON.parseObject(s).toJavaObject(MenPai.class);
+        for (XinFa xinFa : menPai.getXinFas()) {
+            for (Skill skill : xinFa.getSkills()) {
+                for (SkillType skillType : skill.getSkillTypes()) {
+                    SkillType skillType1 = collect.get(skillType.getTitle());
+                    if (skillType1 == null) {
+                        System.out.println(skillType.getTitle());
+                    }
+                    skillType.setTypeId(skillType1.getTypeId());
+                }
+            }
+        }
         System.out.println(JSON.toJSONString(menPai));
+        transaction.commit();
 
-//        SkillType[] skillTypes = new SkillType[a.length];
-//        for (int i = 0; i < skillTypes.length; i++) {
-//            skillTypes[i] = new SkillType();
-//            skillTypes[i].setTitle(a[i]);
-//            session.save(skillTypes[i]);
-//        }
-//        MenPai menPai=new MenPai();
-//        menPai.setName("纯阳");
-//        transaction.commit();
-//        System.out.println(commentService.findCommentByArticleId(16));
-//        Comment comment=new Comment();
-//        User user=new User();
-//        user.setUserId(1);
-//        BaseArticle yzlin = new BaseArticle();
-//        yzlin.setArticleId(16);
-//        comment.setArticle(yzlin);
-//        comment.setUser(user);
-//        comment.setContent("<p>a</p>");
-//        commentService.saveComment(comment);
+    }
 
+    @Test
+    public void findQiXueGroup() {
+        Session session = sessionFactory.openSession();
+//        Transaction transaction = session.beginTransaction();
+        Query<QiXueGroup> query = session.createQuery("from QiXueGroup where qiXueGroupId>335", QiXueGroup.class);
+        List<QiXueGroup> list = query.list();
+        System.out.println(JSON.toJSONString(list));
+
+
+    }
+
+    @Test
+    public void saveQiXueGroup() throws IOException {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        String s = FileUtils.readFileToString(ResourceUtils.getFile("classpath:data/menpai/铁牢奇穴.json"), "utf-8");
+        List<QiXueGroup> qiXueGroups = JSON.parseArray(s).toJavaList(QiXueGroup.class);
+        qiXueGroups.forEach(session::save);
+        transaction.commit();
     }
 
     private void setSkillTypeId(Session session) throws IOException {
@@ -96,6 +123,28 @@ public class TestUnit {
             }
         }
         System.out.println(JSON.toJSONString(qiXueGroups));
+    }
 
+    @Test
+    public void setQiXueType() throws IOException {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        List<SkillType> skillTypeList = session.createQuery("from SkillType ", SkillType.class).list();
+        Map<String, SkillType> collect = skillTypeList.stream().collect(Collectors.toMap(SkillType::getTitle, v -> v));
+        String s = FileUtils.readFileToString(ResourceUtils.getFile("classpath:data/menpai/铁牢奇穴.json"), "utf-8");
+        List<QiXueGroup> qiXueGroups = JSON.parseArray(s).toJavaList(QiXueGroup.class);
+        for (QiXueGroup qiXueGroup : qiXueGroups) {
+            for (QiXue qiXue : qiXueGroup.getQiXues()) {
+                for (SkillType skillType : qiXue.getSkillTypes()) {
+                    SkillType skillType1 = collect.get(skillType.getTitle());
+                    if (skillType1 == null) {
+                        System.out.println(skillType.getTitle());
+                    }
+                    skillType.setTypeId(skillType1.getTypeId());
+                }
+            }
+        }
+        System.out.println(JSON.toJSONString(qiXueGroups));
+        transaction.commit();
     }
 }
