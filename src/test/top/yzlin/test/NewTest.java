83ff7c03a5d5e23c87bd456ef362689cbbdb7761
yzlin <499680328@qq.com>
2019-06-11 16:11:44 +0800

    更新所有奇穴图片 更新藏剑技能图片

--- src/test/top/yzlin/test/NewTest.java
+++ src/test/top/yzlin/test/NewTest.java
@@ -1,5 +1,7 @@
 package top.yzlin.test;
 
+import com.alibaba.fastjson.JSONObject;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import top.yzlin.jx3strategystation.entity.game.Skill;
+import top.yzlin.jx3strategystation.entity.game.XinFa;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:applicationConfig.xml"})
@@ -16,8 +20,19 @@ public class NewTest {
     @Autowired
     private SessionFactory sessionFactory;
 
+//    @Value("#{imgPath}")
+//    private String imgPath;
+
     @Test
     public void test() {
+        Session session = sessionFactory.openSession();
+        XinFa xinFa = session.get(XinFa.class, 172);
+        JSONObject jo = new JSONObject();
+        for (Skill skill : xinFa.getSkills()) {
+            jo.put(skill.getName(), "");
+        }
+        System.out.println(jo);
 
+//        session.get(QiXue.class, 95)
     }
 }
