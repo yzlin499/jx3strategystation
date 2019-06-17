1ac0db6603106668ae7229ac22fb3d9d7247a1c8
yzlin <499680328@qq.com>
2019-06-17 22:43:31 +0800

    紧急更新数据库

--- src/test/top/yzlin/test/NewTest.java
+++ src/test/top/yzlin/test/NewTest.java
@@ -1,5 +1,7 @@
 package top.yzlin.test;
 
+import com.alibaba.fastjson.JSONObject;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import top.yzlin.jx3strategystation.entity.game.Skill;
+import top.yzlin.jx3strategystation.entity.game.XinFa;
 import top.yzlin.jx3strategystation.service.GameDataService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -25,9 +29,13 @@ public class NewTest {
 
     @Test
     public void test() {
-        gameDataService.findMenPaiXinFasByXinFaName("铁牢律").forEach(i -> {
-            System.out.println(i.getName());
-        });
+        Session session = sessionFactory.openSession();
+        XinFa xinFa = session.get(XinFa.class, 552);
+        JSONObject jo = new JSONObject();
+        for (Skill skill : xinFa.getSkills()) {
+            jo.put(skill.getName(), "");
+        }
+        System.out.println(jo);
 
     }
 }
